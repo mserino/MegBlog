3dcbaa4c693ad6802594bbba7a448830187fc604
mserino <m.serino@hotmail.it>
2014-09-14 19:25:25 +0100

    Post can be edited

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,22 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		@post = Post.create params[:post].permit(:title, :description)
+		@post = Post.create post_params
 		redirect_to posts_path
+	end
+
+	def edit
+		@post = Post.find params[:id]
+	end
+
+	def update
+		@post = Post.find params[:id]
+		@post.update post_params
+		flash[:notice] = "Post successfully updated"
+    redirect_to posts_path
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :description)
 	end
 end
--- spec/features/post_creation_feature_spec.rb
+++ spec/features/post_creation_feature_spec.rb
@@ -1,13 +1,27 @@
 require 'rails_helper'
 
 describe 'Post creation' do
-	it 'can create a post' do
+	before do
 		visit '/posts/new'
 		fill_in 'Title', with: 'Yo'
 		fill_in 'Description', with: 'I am a superverycool post'
 		click_button 'Submit'
+	end
+
+	it 'can create a post' do
 		expect(current_path).to eq posts_path
 		expect(page).to have_content 'Yo'
 		expect(page).to have_content 'I am a superverycool post'
+	end
+
+	it 'can edit a post' do
+		visit '/posts'
+		click_link 'Edit post'
+		fill_in 'Title', with: 'Try again'
+		click_button 'Submit'
+		expect(current_path).to eq posts_path
+		expect(page).to have_content 'Try again'
+		expect(page).not_to have_content 'Yo'
+		expect(page).to have_content 'Post successfully updated'
 	end
 end
